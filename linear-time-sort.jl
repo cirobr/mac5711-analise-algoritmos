a4cb1f5600e3599ef0e2533d8e3bd3a3cf73042f
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-05-03 12:51:38 -0300

    counting_sort passed

--- linear-time-sort.jl
+++ linear-time-sort.jl
@@ -8,8 +8,8 @@ function counting_sort(A::Vector{Int}, k::Int)
     for j in 1:n   C[A[j]] += 1   end
     for i in 2:k   C[i] = C[i] + C[i-1]   end
     for j in n : -1 : 1
-        B[C[A[j]]] = A[j]
-        C[A[j]]    = A[j-1]
+        B[ C[ A[j] ] ] = A[j]
+        C[ A[j] ]      = C[ A[j] ] - 1
     end
     
     return B
