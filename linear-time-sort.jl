1b0da0e3e61bc7b5f1b7eec32bf99e37d8defeaa
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-05-03 19:15:11 -0300

    Update linear-time-sort.jl

--- linear-time-sort.jl
+++ linear-time-sort.jl
@@ -5,7 +5,7 @@ function counting_sort(A::Vector{Int}, k::Int)
     n = size(A)[1]
     B = zeros(Int, n)
     C = zeros(Int, k)                           # k valor máximo de elemento contido em A
-    for j in 1:n   C[A[j]] += 1   end
+    for j in 1:n   C[ A[j] ] += 1         end
     for i in 2:k   C[i] = C[i] + C[i-1]   end
     for j in n : -1 : 1
         B[ C[ A[j] ] ] = A[j]                   # estabilidade do counting_sort
