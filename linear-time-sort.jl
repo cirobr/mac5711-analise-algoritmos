de3327a7ef9a4b71757fb10e8e6ca407a1163bbc
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-05-03 13:11:05 -0300

    added comments

--- linear-time-sort.jl
+++ linear-time-sort.jl
@@ -1,15 +1,15 @@
 # CLRS page 141
 # stable algorithm
-# O(n)
+# theta(k + n)
 function counting_sort(A::Vector{Int}, k::Int)
     n = size(A)[1]
     B = zeros(Int, n)
-    C = zeros(Int, k)
+    C = zeros(Int, k)                           # k valor máximo de elemento contido em A
     for j in 1:n   C[A[j]] += 1   end
     for i in 2:k   C[i] = C[i] + C[i-1]   end
     for j in n : -1 : 1
-        B[ C[ A[j] ] ] = A[j]
-        C[ A[j] ]      = C[ A[j] ] - 1
+        B[ C[ A[j] ] ] = A[j]                   # estabilidade do counting_sort
+        C[ A[j] ]      = C[ A[j] ] - 1          # estabilidade do counting_sort
     end
     
     return B
